e5ae9b66342d9dbe196845d239e01607c161bf0f
myid-hana <sosqkrgksml@gmail.com>
2021-01-06 10:40:36 +0900

    - App.js, App.scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Header from './components/Header';
+import './App.scss';
+import '@csstools/normalize.css';
+
 function App() {
-  return <div className="App">dmdslfjs</div>;
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
